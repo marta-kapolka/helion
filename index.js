1db74bd74e691263f615ef6b30dcc1ec4f223d68
marta-kapolka <mkapolka@gmail.com>
2021-08-19 15:00:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const list = [
+  {
+    id: 1,
+    polishTitle: "tytuł",
+    numberOfPages: 175,
+    releaseDate: "2019-05-28",
+  },
+  {
+    id: 2,
+    polishTitle: "tytuł jeszcze jakiś",
+    numberOfPages: 283,
+    releaseDate: "2021-03-17",
+  },
+  {
+    id: 3,
+    polishTitle: "kolejny tytuł",
+    numberOfPages: 93,
+    releaseDate: "2015-10-03",
+  },
+  {
+    id: 4,
+    polishTitle: "coś jeszcze napisali",
+    numberOfPages: 127,
+    releaseDate: "2018-02-26",
+  },
+];
+
+console.log(list);
